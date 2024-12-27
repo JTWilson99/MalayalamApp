3799f1672b8dc77a7b6e856426839c4578cd378f
JTWilson <jcub.wilson@yahoo.ca>
2024-12-27 13:07:07 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './Script_Icon.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={img} className="App-logo" alt="logo" />
+        <p>
+          Practice writing and reading!
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+function Module({ Icon, text }) {
+  return (
+    
+
+  );
+}
+
+export default App;
